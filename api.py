acc5ffa84d4a59c8612bc87a323bdff722b73160
Kevin Kunzler <47984999+kevinalejandre999@users.noreply.github.com>
2025-06-12 20:40:53 -0300

    Update api.py
    Correcion del puerto

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -18,4 +19,5 @@ def update_senal(color, hora):
     return jsonify({"mensaje": "Señal actualizada correctamente", "senal": ultima_senal}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
